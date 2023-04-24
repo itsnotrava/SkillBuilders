821e292ae8aef47f31614d0740a174d9dba4f3a1
Nicola Travaglini <nicola1.travaglini@gmail.com>
2023-04-24 11:42:59 +0200

    Servlet modifiche

--- src/main/java/servlet/ServletAccesso.java
+++ src/main/java/servlet/ServletAccesso.java
@@ -1,17 +1,15 @@
 package servlet;
 
 import java.io.*;
-import java.sql.SQLException;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import dao.UtenzeDao;
-import exception.EmailAlreadyTakenException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "registrazione", value = "/registrazione")
-public class ServletRegistrazione extends HttpServlet {
+@WebServlet(name = "accesso", value = "/accesso")
+public class ServletAccesso extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.addHeader("Access-Control-Allow-Origin", "*");
@@ -21,27 +19,20 @@ public class ServletRegistrazione extends HttpServlet {
 		JsonObject temp = new Gson().fromJson(body, JsonObject.class);
 		// fromJason => trasforma da stringa a Json, prende in input -stringa- -tipo destinazione-
 
-		String nome = temp.get("nome").toString(); // TROVO IL NOME
-		String password = temp.get("password").toString(); // TROVO LA PASSWORD
-		String email = temp.get("email").toString(); // TROVO LA MAIL
-		String anno = temp.get("anno").toString(); // TROVO IL NOME
-		String indirizzo = temp.get("indirizzo").toString(); // TROVO L'INDIRIZZO
-		String sezione = temp.get("sezione").toString(); // TROVO LA SEZIONE
-		String quartiere = temp.get("quaetiere").toString(); // TROVO LA POSIZIONE
-
-
-		JsonObject responseJson = new JsonObject();
+		String nome = temp.get("email").toString(); // TROVO IL NOME (email)
+		String password = temp.get("password").toString(); // TROVO IL NOME (email)
+        JsonObject responseJson = new JsonObject();
 		responseJson.addProperty("risultato", "sucesso!");
-
 
 		// Invio il risultato al client
 		PrintWriter printWriter = response.getWriter();
 		printWriter.println(responseJson.toString());
 		printWriter.flush();
+
 	}
 
-
-	private static String getBody(HttpServletRequest request) throws IOException {
+	// PRESA DA INTERNET, SI OCCUPA DI FARE IL BODY DELLA RICHIESTAA
+	public static String getBody(HttpServletRequest request) throws IOException {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader reader = request.getReader();
 		String line;
@@ -50,4 +41,5 @@ public class ServletRegistrazione extends HttpServlet {
 		}
 		return sb.toString();
 	}
+
 }
