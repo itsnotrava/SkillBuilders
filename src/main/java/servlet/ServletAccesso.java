064556ef23d71152c428edc1d8af793b6084d63e
Nicola Travaglini <nicola1.travaglini@gmail.com>
2023-04-24 11:42:59 +0200

    Stesura ServletAccesso.java

---
+++ src/main/java/servlet/ServletAccesso.java
@@ -0,0 +1,11 @@
+package servlet;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "accesso", value = "/accesso")
+public class ServletAccesso extends HttpServlet {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+	}
+}
