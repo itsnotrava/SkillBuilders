8afc16ad55043bc2164fff4ce8d2e2840597d280
nicuj <ion.jalba@aldini.istruzioneer.it>
2023-05-11 20:31:45 +0200

    LogOut

---
+++ src/main/java/servlet/ServletLogOut.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import java.io.*;
+import java.sql.SQLException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dao.SkillBuildersDao;
+import exceptions.EmailOPasswordErrati;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "LogOut", value = "/LogOut")
+public class ServletLogOut extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+
+
+        HttpSession session = request.getSession();
+        session.invalidate();
+    }
+}
