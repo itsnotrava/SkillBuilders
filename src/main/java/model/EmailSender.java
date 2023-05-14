ccf2773a7e360da9de3f6d479e97d44c4a294f1c
Nicola Travaglini <nicola1.travaglini@gmail.com>
2023-05-14 18:29:02 +0200

    Tentativo di risolvere il problema dell'invio della email usando Amazon SES

--- src/main/java/model/EmailSender.java
+++ src/main/java/model/EmailSender.java
@@ -9,9 +9,10 @@ import jakarta.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class EmailSender {
-	private final String emailFrom = "nicola1.travaglini@gmail.com";
-	private final String passwordFrom = "xtkicxlycjaprlpm";
-	private final String providerFrom = "smtp.gmail.com";
+	private final String emailFrom = "AKIAQUOFW57HDGAXJLZZ";
+	private final String passwordFrom = "BJbvk0wzBybbPdjEx8/wJTX68OmRlRtUt/2Joqms4CyX";
+	private final String providerFrom = "email-smtp.eu-north-1.amazonaws.com";
+	private final int port = 587;
 	private String emailTo;
 	private int otp;
 
@@ -24,13 +25,18 @@ public class EmailSender {
 		Properties props = new Properties();
 		props.put("mail.smtp.host", this.providerFrom);
 		props.put("mail.smtp.starttls.enable", "true");
-		Session session = Session.getInstance(props, null);
+		props.put("mail.smtp.port", this.port);
+		props.put("mail.smtp.auth", "true");
+
+		Session session = Session.getDefaultInstance(props);
 
 		MimeMessage msg = new MimeMessage(session);
 		msg.setFrom(emailFrom);
 		msg.setRecipients(Message.RecipientType.TO, emailTo);
 		msg.setSubject("OTP SkillBuilders");
 		msg.setText(Integer.toString(otp));
-		Transport.send(msg, emailFrom, passwordFrom);
+		Transport transport = session.getTransport();
+		transport.connect(providerFrom, emailFrom, passwordFrom);
+		transport.sendMessage(msg, msg.getAllRecipients());
 	}
 }
